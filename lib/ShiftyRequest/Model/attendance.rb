6a0a86c0181617c65d7b998730ae523ad5f6dda2
dongcheol.park <dongcheol.park@privatenote.co.kr>
2023-07-13 17:00:42 +0900

    add work_time

--- lib/ShiftyRequest/Model/attendance.rb
+++ lib/ShiftyRequest/Model/attendance.rb
@@ -17,6 +17,10 @@ module ShiftyRequest
       def proper_time?(start_at: DateTime.new(2000, 1, 1, 10, 0, 0, '+9'), end_at: DateTime.new(2000, 1, 1, 19, 0, 0, '+9'))
         @clock_in_time.strftime('%H%M%S%N') <= start_at.strftime('%H%M%S%N') && end_at.strftime('%H%M%S%N') <= @clock_out_time.strftime('%H%M%S%N')
       end
+
+      def work_time
+        clock_out_time - clock_in_time
+      end
     end
   end
 end
--- spec/model/attendance_spec.rb
+++ spec/model/attendance_spec.rb
@@ -39,4 +39,34 @@ RSpec.describe 'attendance test' do
       expect(attendance.proper_time?).to be_falsey
     end
   end
+
+  describe 'work_time 메소드' do
+    it '2 시간' do
+      attendance = ShiftyRequest::Model::Attendance.new(
+        {
+          attendance_id: '1234',
+          clock_in_time: '2023-06-05T10:00:00+09:00',
+          clock_out_time: '2023-06-05T12:00:00+09:00',
+        }
+      )
+
+      work_time = attendance.work_time
+
+      expect(work_time).to eq(Rational(2, 24))
+    end
+
+    it '9 시간' do
+      attendance = ShiftyRequest::Model::Attendance.new(
+        {
+          attendance_id: '1234',
+          clock_in_time: '2023-06-05T10:00:00+09:00',
+          clock_out_time: '2023-06-05T19:00:00+09:00',
+        }
+      )
+
+      work_time = attendance.work_time
+
+      expect(work_time).to eq(Rational(9, 24))
+    end
+  end
 end
