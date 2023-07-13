e5702d4b844fc67f344be828220b60f816a77d09
dongcheol.park <dongcheol.park@privatenote.co.kr>
2023-07-13 15:16:54 +0900

    load env

--- lib/ShiftyRequest/load_attendance.rb
+++ lib/ShiftyRequest/load_attendance.rb
@@ -8,7 +8,6 @@ module ShiftyRequest
 
     def call
       response = HTTParty.post(url, headers: headers, body: body.to_json)
-      puts response
 
       parse_response_body(body: JSON.parse(response.body))
     end
@@ -22,7 +21,7 @@ module ShiftyRequest
       {
         "attendances": {
           "date_ranges": [['2023-06-24T00:00:00+09:00', '2023-08-07T23:59:59+09:00']],
-          "employee_ids": [713220],
+          "employee_ids": [ENV['EMPLOYEE_ID'].to_i],
         },
       }
     end
